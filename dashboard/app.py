0c663b6073b73223bed516d0f159ad164e2a664b
Reem Naji <reemalee7@gmail.com>
2026-07-06 00:29:41 +0300

    final dashboard i promise

--- dashboard/app.py
+++ dashboard/app.py
@@ -33,30 +33,6 @@ st.title("2026 WNBA MVP Race Predictions & Analytics 🏀 ")
 
 st.markdown("---")
 leader_name = data.iloc[0]['Player']
-
-# --- ROW 1: MVP Frontrunner & TOP 5 MVP LEADERS  ---
-col1, col2 = st.columns([1, 2])
-
-
-with col1:
-    
-    st.subheader("🏆 MVP Frontrunner")
-    leader_share = data.iloc[0]['Predicted_Share']
-    st.metric(label="",value=leader_name, delta=f"{leader_share:.2%} Pred. Share")
-
-with col2:
-
-    st.subheader("Top 5 MVP Contenders")
-    st.markdown("Current leaders based on predicted award share:")
-
-    top_5 = data[['Player', 'Team', 'Pos', 'Predicted_Share']].head(5)
-    
-    st.dataframe(
-        top_5.style.format({'Predicted_Share': '{:.4f}'}),
-        use_container_width=True,
-        hide_index=True
-    )
-
 features = [
     ('pts_per_game', 'Points per game'),
     ('ast_per_game', 'Assists Per Game'),
@@ -69,16 +45,44 @@ features = [
     ('Predicted_Share', 'Predicted Share'),
 ]
 
-# --- ROW 2: HEATMAP AND INTERACTIVE SCATTER PLOTS  ---
-st.markdown("---")
-col3, col4 = st.columns([1,1], gap="xxlarge")
+col1, col2, col3 = st.columns([0.5, 1, 1], gap="medium")
+
+with col1:
+    
+    st.subheader("Predicted MVP 🏆")
+    leader_share = data.iloc[0]['Predicted_Share']
+    st.metric(label="mvp frontrunner",value=leader_name, delta=f"{leader_share:.2%} Pred. Share", label_visibility="hidden")
+
+with col2:
+
+    st.subheader("Top 5 Predicted MVP Contenders")
+    st.markdown("Current leaders based on predicted award share:")
+
+    top_5 = data[['Player', 'Team', 'Pos', 'Predicted_Share']].head(5)
+    
+    fig_top5 = px.scatter(
+            top_5, 
+            x='Predicted_Share', 
+            y='Player',
+            size='Predicted_Share', 
+            color_discrete_sequence=['#90D743'],
+            hover_data=['Team', 'Pos', 'Predicted_Share']
+        )
+    
+    fig_top5.update_layout(
+        margin=dict(l=10, r=20, t=20, b=10),
+        height=280,
+        xaxis_title="Predicted Share",
+        yaxis_title="Player",
+    )
+    
+    st.plotly_chart(fig_top5, use_container_width=True)
 
 with col3:
 
     st.subheader("Which Stats Matter Most to the Model?")
     st.markdown("The statistics most closely linked to a high predicted award share")
 
-    # 4. Create the Interactive Plotly Bar Chart
     fig_corr_bar = px.bar(
         feature_importance,
         x='feature',
@@ -88,15 +92,29 @@ with col3:
         labels={'mean_abs_shap': 'Feature Importance (Shap)'}
     )
     
-    # 5. Layout optimizations (mobile-friendly and clean)
     fig_corr_bar.update_layout(
         margin=dict(l=10, r=10, t=10, b=10), 
-        height=320,
-        xaxis=dict(tickangle=-45, title=None), # Angles feature names so they don't overlap
-        coloraxis_showscale=False              # Hides the color legend to save space (since Y-axis already shows the value)
+        height=350,
+        xaxis=dict(tickangle=-45, title=None), 
+        coloraxis_colorbar=dict(
+                orientation="h",  
+                yanchor="bottom",
+                y=-1,            
+                xanchor="center",
+                x=0.5,
+                thickness=5,       
+                title=dict(
+                text="Feature Importance",
+                side="top",
+            )          
+            )   
     )
     
     st.plotly_chart(fig_corr_bar, use_container_width=True)
+
+st.markdown("---")
+
+col4, col5 = st.columns([1,1], gap="xxlarge")
 
 with col4:
 
@@ -108,7 +126,7 @@ with col4:
     selected_feat = [f[0] for f in features if f[1] == selected_feat_label][0]
 
     fig = px.scatter(
-        data, y=selected_feat, x='WS',
+        data, y='WS', x=selected_feat,
         color='Predicted_Share', hover_name='Player',
         hover_data=['Team', 'Pos', 'Predicted_Share'],
         color_continuous_scale='viridis',
@@ -116,7 +134,7 @@ with col4:
     )
     fig.update_layout(
             margin=dict(l=10, r=10, t=25, b=10), 
-            height=320, # Slightly taller to make room for the bottom colorbar
+            height=400,
             coloraxis_colorbar=dict(
                 orientation="h",    # Make colorbar horizontal
                 yanchor="bottom",
@@ -132,22 +150,17 @@ with col4:
         )
     st.plotly_chart(fig, use_container_width=True)
     
-st.markdown("---")
-
-    # --- ROW 3: FEATURE DISTRIBUTIONS ---
-
-with st.container():
+with col5:
     st.subheader("MVP-Caliber Players vs. the Rest of the League")
     st.markdown(f"How the top 9 MVP-caliber players — and {leader_name} in particular — separate from the rest of the league")
 
-    # 1. Feature selection (Keep at the top)
     features_ = features
     features_.insert(0,('WS', 'Win Shares (WS)'))
     numeric_columns = [f[1] for f in features_]
     selected_feat_label = st.selectbox("Select a metric to explore:", options=numeric_columns, index=0)
     selected_feat = [f[0] for f in features_ if f[1] == selected_feat_label][0]
 
-    # 2. Setup Pills for MULTIPLE selections
+    # Setup Pills for MULTIPLE selections
     tags = ['MVP Caliber', 'Rest of League']
     
     # selection_mode="multi" allows the user to click both pills. 
@@ -164,7 +177,7 @@ with st.container():
     plot_data = data.copy()
     plot_data['Player Tier'] = plot_data['mvp_caliber'].apply(lambda x: 'MVP Caliber' if x == 'yes' else 'Rest of League')
 
-    # 3. Dynamic Logic for Filtering and Coloring
+    # Dynamic Logic for Filtering and Coloring
     if not selected_tags:
         # Condition A: None selected -> Show whole league as one solid color
         filtered_data = plot_data
@@ -178,7 +191,6 @@ with st.container():
         filtered_data = plot_data[plot_data['Player Tier'] == selected_tags[0]]
         color_col = None
 
-    # 4. Create the Histogram
     fig_detailed_hist = px.histogram(
         filtered_data, 
         x=selected_feat, 
@@ -188,7 +200,6 @@ with st.container():
         marginal="box",
         hover_data=['Player', 'Team'], 
         
-        # Custom color mappings: MVP (Gold/Orange), Rest of League (Blue). Solid Blue if single tag.
         color_discrete_map={'MVP Caliber': '#90D743', 'Rest of League': '#477E9F'} if color_col else None,
         color_discrete_sequence=["#31688E"] if not color_col else None
     )
@@ -197,7 +208,7 @@ with st.container():
     fig_detailed_hist.update_traces(opacity=0.75)
     fig_detailed_hist.update_layout(
         bargap=0.05, 
-        height=450, # Increased height slightly so the boxplot has room to breathe
+        height=400, # Increased height slightly so the boxplot has room to breathe
         margin=dict(l=10, r=10, t=30, b=10),
         legend=dict(
             orientation="h",      # Make legend horizontal
@@ -210,3 +221,5 @@ with st.container():
     )
 
     st.plotly_chart(fig_detailed_hist, use_container_width=True)
+
+st.markdown("---")
